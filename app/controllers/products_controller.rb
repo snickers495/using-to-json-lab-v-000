3c8f30ab4c6dcd9d8264944bd702e1eb3b47dd9c
Lisa Lee <snickers495@gmail.com>
2018-03-09 03:46:44 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
-    render json: product.to_json(only: :id, :name, :title)
+    render json: product.to_json(only: :id, :name, :description)
   end
 
   private
